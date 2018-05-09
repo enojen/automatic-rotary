efd0df3d7f222e92192bcc5998d59188dc2ea22f
Kerem Can <fuckirama@icloud.com>
2018-05-09 16:08:39 +0300

    dosya kullanıcıdan alınıyor, taglama ve linkleme çalışıyor. linkleme için variable değiştirmesi yapılmadı

---
+++ es.py
@@ -0,0 +1,34 @@
+from elasticsearch import Elasticsearch
+client = Elasticsearch()
+
+#abc = "Anasayfa, seçkin maddelerden birinin giriş bölümünün sergilendiği bir bölüme sahiptir. Bu bölümde, okuyuculara Vikipedi'nin ürettiği en başarılı içerik örnekleri sergilenmektedir. Bu ayın anasayfaya çıkan seçkin madde listesi buradan görülebilir. Maddenin anasayfaya çıkabilmesi için seçkin madde statüsünde olması gerekmektedir (herhangi bir maddeyi aday göstermek için bkz: Vikipedi:Seçkin madde adayları). Seçkin madde statüsü alan maddeler, seçilme sıralarına göre kronolojik olarak anasayfaya çıkarlar. Seçkin içerik sorumluları, anasayfaya çıkacak olan maddelerin düzeninden ve sistemin sağlıklı işleyişinden sorumludur. Konuyla ilgili bilgi almak veya herhangi bir sorun bildirmek için lütfen sorumluların sayfasına mesaj bırakın maddelerden."
+userdoc = input('Please enter your document: ')
+response = client.search(
+    index="my-index",
+    body={
+      "query" : {
+        "percolate" : {
+            "field": "query",
+            "document" : {
+                "message" : userdoc
+            }
+        }
+    },
+    "highlight": {
+      "pre_tags" : ["<a href='hyperlinkvariable'>"],
+      "post_tags" : ["</a>"],
+      "fields": {
+        "message": {
+          "number_of_fragments" : 3,
+          "fragment_size" : 1500
+          
+        }
+      }
+    }
+    }
+)
+
+for hit in response['hits']['hits']:
+    a = str(hit['highlight']['message'])
+    y = a.replace("</a> <a href='hyperlinkvariable'>", " ").replace("hyperlinkvariable", "https://google.com")
+    print(y)
