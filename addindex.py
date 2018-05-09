d88828b8869f9935f3bf291973c2d5562f1c1eb8
Kerem Can <fuckirama@icloud.com>
2018-05-09 20:57:22 +0300

    index oluşturma, titleları ekleme

---
+++ addindex.py
@@ -0,0 +1,52 @@
+import json
+
+from elasticsearch_dsl import (
+    connections,
+    DocType,
+    Mapping,
+    Percolator,
+    Text
+)
+from elasticsearch_dsl.query import (
+    SpanNear,
+    SpanTerm
+)
+from elasticsearch import Elasticsearch
+
+# Read the json File
+json_data = open('titles.json').read()
+data = json.loads(json_data)
+
+docs = data['response']['docs']
+
+# creating a new default elasticsearch connection
+connections.configure(
+    default={'hosts': 'localhost:9200'},
+)
+
+
+class Document(DocType):
+    title = Text()
+    query = Percolator()
+
+    class Meta:
+        index = 'my-index'
+        doc_type = '_doc'
+
+    def save(self, **kwargs):
+        return super(Document, self).save(**kwargs)
+
+
+# create the mappings in elasticsearch
+Document.init()
+
+# index the query
+for doc in docs:
+    terms = doc['title'].split(" ")
+    clauses = []
+    for term in terms:
+        field = SpanTerm(title=term)
+        clauses.append(field)
+    query = SpanNear(clauses=clauses)
+    item = Document(query=query)
+    item.save()
