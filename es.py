87879fe93363e71c62c86d6bf1fe2b22f244d5a6
Kerem Can <fuckirama@icloud.com>
2018-05-09 17:02:08 +0300

    kod düzenlemesi

--- es.py
+++ es.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 
 client = Elasticsearch()
 
-
 #abc = "Anasayfa, seçkin maddelerden birinin giriş bölümünün sergilendiği bir bölüme sahiptir. Bu bölümde, okuyuculara Vikipedi'nin ürettiği en başarılı içerik örnekleri sergilenmektedir. Bu ayın anasayfaya çıkan seçkin madde listesi buradan görülebilir. Maddenin anasayfaya çıkabilmesi için seçkin madde statüsünde olması gerekmektedir (herhangi bir maddeyi aday göstermek için bkz: Vikipedi:Seçkin madde adayları). Seçkin madde statüsü alan maddeler, seçilme sıralarına göre kronolojik olarak anasayfaya çıkarlar. Seçkin içerik sorumluları, anasayfaya çıkacak olan maddelerin düzeninden ve sistemin sağlıklı işleyişinden sorumludur. Konuyla ilgili bilgi almak veya herhangi bir sorun bildirmek için lütfen sorumluların sayfasına mesaj bırakın maddelerden."
 userdoc = input('Please enter your document: ')
 response = client.search(
@@ -23,7 +22,7 @@ response = client.search(
       "fields": {
         "message": {
           "number_of_fragments" : 3,
-          "fragment_size" : 1500
+          "fragment_size" : 2500
           
         }
       }
