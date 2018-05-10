5f59cc9b129aa06cbf5eead46643fa412400e081
Kerem Can <fuckirama@icloud.com>
2018-05-11 00:56:37 +0300

    elastic methodu düzenleme

--- es.py
+++ es.py
@@ -41,21 +41,21 @@ def elastic():
     }
   )
   for hit in response['hits']['hits']:
-    a = str(hit['highlight']['title'])
-    y = a.replace("</a> <a href='hyperlinkvariable'>", " ")
-    soup = BeautifulSoup(y, 'html.parser')
+    title = str(hit['highlight']['title'])
+    title = title.replace("</a> <a href='hyperlinkvariable'>", " ")
+    soup = BeautifulSoup(title, 'html.parser')
     hyperlink = soup.a.string
     t = hyperlink.replace("ş", "s").replace("ç", "c").replace(" ", "-").replace("ı", "i").replace("ğ", "g").replace(" ", "-").replace("ö", "o").replace("ü", "u").replace("Ş", "ş").replace("Ç", "ç").replace("İ", "i").replace("Ü", "ü").replace("Ö", "ö").replace("(", "").replace(")", "")
-    k = y.replace("hyperlinkvariable", ("http://127.0.0.1/" + t))
-    f(k)
-def f(k):
+    title = title.replace("hyperlinkvariable", ("http://127.0.0.1/" + t))
+    f(title)
+def f(title):
     global arr
-    soup1 = BeautifulSoup(k, 'html.parser')
+    global userdoc
+    soup1 = BeautifulSoup(title, 'html.parser')
     withhyperlink = soup1.a
     plaintext = soup1.a.string
     str_link = str(withhyperlink)
-    str_text = str(plaintext)
-    global userdoc
+    str_text = str(plaintext)    
     userdoc = userdoc.replace(str_text, str_link)
     userdoc = userdoc.replace(" , ", ", ")
     return arr.append(userdoc)
