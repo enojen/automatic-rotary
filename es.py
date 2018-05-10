a633292d03abded770b3187bdeb8a339426cb8cd
Kerem Can <fuckirama@icloud.com>
2018-05-11 00:50:42 +0300

    kod düzenlemesi yapıldı, kodlar methodlara dönüştürüldü

--- es.py
+++ es.py
@@ -2,25 +2,20 @@ from elasticsearch import Elasticsearch
 from bs4 import BeautifulSoup
 import webbrowser
 
-
+userdoc = ""
 arr = []
-userdoc = input('Please enter your document: ')
-userdoc = userdoc.replace(",", " , ")
-client = Elasticsearch()
-def f(k):
-    global arr
-    soup1 = BeautifulSoup(k, 'html.parser')
-    withhyperlink = soup1.a
-    plaintext = soup1.a.string
-    str_link = str(withhyperlink)
-    str_text = str(plaintext)
-    global userdoc
-    userdoc = userdoc.replace(str_text, str_link)
-    userdoc = userdoc.replace(" , ", ", ")
-    return arr.append(userdoc)
-#abc = "Anasayfa, seçkin maddelerden birinin giriş bölümünün sergilendiği bir bölüme sahiptir. Bu bölümde, okuyuculara Vikipedi'nin ürettiği en başarılı içerik örnekleri sergilenmektedir. Bu ayın anasayfaya çıkan seçkin madde listesi buradan görülebilir. Maddenin anasayfaya çıkabilmesi için seçkin madde statüsünde olması gerekmektedir (herhangi bir maddeyi aday göstermek için bkz: Vikipedi:Seçkin madde adayları). Seçkin madde statüsü alan maddeler, seçilme sıralarına göre kronolojik olarak anasayfaya çıkarlar. Seçkin içerik sorumluları, anasayfaya çıkacak olan maddelerin düzeninden ve sistemin sağlıklı işleyişinden sorumludur. Konuyla ilgili bilgi almak veya herhangi bir sorun bildirmek için lütfen sorumluların sayfasına mesaj bırakın maddelerden."
+def ask_input(a):
+  global userdoc
+  while(not userdoc):
+    userdoc = input(a)
+  #userdoc = userdoc.replace(",", " , ")
+  print(userdoc)
+  return
 
-response = client.search(
+def elastic():
+  global userdoc
+  client = Elasticsearch()
+  response = client.search(
     index="my-index",
     body={
       "query" : {
@@ -44,9 +39,8 @@ response = client.search(
       }
     }
     }
-)
-
-for hit in response['hits']['hits']:
+  )
+  for hit in response['hits']['hits']:
     a = str(hit['highlight']['title'])
     y = a.replace("</a> <a href='hyperlinkvariable'>", " ")
     soup = BeautifulSoup(y, 'html.parser')
@@ -54,14 +48,30 @@ for hit in response['hits']['hits']:
     t = hyperlink.replace("ş", "s").replace("ç", "c").replace(" ", "-").replace("ı", "i").replace("ğ", "g").replace(" ", "-").replace("ö", "o").replace("ü", "u").replace("Ş", "ş").replace("Ç", "ç").replace("İ", "i").replace("Ü", "ü").replace("Ö", "ö").replace("(", "").replace(")", "")
     k = y.replace("hyperlinkvariable", ("http://127.0.0.1/" + t))
     f(k)
-
-
-def main():
+def f(k):
     global arr
-    f = open('index.html','w')
-    message = arr[len(arr)-1]
-    f.write(message)
-    f.close()
-    filename = 'file:///Users/kerem/Desktop/es/index.html'
-    webbrowser.open_new_tab(filename)
-main()
+    soup1 = BeautifulSoup(k, 'html.parser')
+    withhyperlink = soup1.a
+    plaintext = soup1.a.string
+    str_link = str(withhyperlink)
+    str_text = str(plaintext)
+    global userdoc
+    userdoc = userdoc.replace(str_text, str_link)
+    userdoc = userdoc.replace(" , ", ", ")
+    return arr.append(userdoc)
+
+def write_html():
+    global arr
+    try:
+      f = open('index.html','w')
+      message = arr[len(arr)-1]
+      f.write(message)
+      f.close()
+      filename = 'file:///Users/kerem/Desktop/es/index.html'
+      webbrowser.open_new_tab(filename)
+    except:
+      print("No result!")
+
+ask_input("Please enter your document: ")
+elastic()
+write_html()
