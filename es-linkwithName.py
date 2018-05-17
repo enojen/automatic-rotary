294afc71f1692d56316c6b169e15b45464146fe7
enojen <enesozel.eo@gmail.com>
2018-05-17 21:15:04 +0300

    gui yapıldı

--- es-linkwithName.py
+++ es-linkwithName.py
@@ -1,20 +1,26 @@
+#coding:utf8
+#!/usr/bin/env python # -*- coding: latin-1 -*-
 from elasticsearch import Elasticsearch
 from bs4 import BeautifulSoup
-import webbrowser, os
+import webbrowser
+import os
 
-userdoc = ""
+
+from flask import Flask, request, render_template
+app = Flask(__name__)
+
 arr = []
 
 
-def ask_input():
-    global userdoc    
-    f = open("text.txt", "r")
-    userdoc = f.read()          
-    userdoc = userdoc.replace(",", " , ")
+@app.route('/')
+def my_form():
+    return render_template('my-form.html')
 
 
-def elastic():
-    global userdoc
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    userdoc = text.replace(",", " , ")
     client = Elasticsearch()
     response = client.search(
         index="my-index",
@@ -49,41 +55,15 @@ def elastic():
         t = hyperlink.replace("ş", "s").replace("ç", "c").replace(" ", "-").replace("ı", "i").replace("ğ", "g").replace(" ", "-").replace("ö", "o").replace(
             "ü", "u").replace("Ş", "ş").replace("Ç", "ç").replace("İ", "i").replace("Ü", "ü").replace("Ö", "ö").replace("(", "").replace(")", "").replace("â", "a")
         title = title.replace("hyperlinkvariable", ("http://127.0.0.1/" + t))
-        f(title)
+        soup1 = BeautifulSoup(title, 'html.parser')
+        withhyperlink = soup1.a
+        plaintext = soup1.a.string
+        str_link = str(withhyperlink)
+        str_text = str(plaintext)
+        userdoc = userdoc.replace(str_text, str_link)
+        userdoc = userdoc.replace(" , ", ", ")
 
+        arr.append(userdoc)
+    message = arr[len(arr)-1]
 
-def f(title):
-    global arr
-    global userdoc
-    soup1 = BeautifulSoup(title, 'html.parser')
-    withhyperlink = soup1.a
-    plaintext = soup1.a.string
-    str_link = str(withhyperlink)
-    str_text = str(plaintext)
-    userdoc = userdoc.replace(str_text, str_link)
-    userdoc = userdoc.replace(" , ", ", ")
-    return arr.append(userdoc)
-
-
-def write_html():
-    global arr
-    try:
-        f = open('index.html', 'w')
-        message = arr[len(arr)-1]
-        f.write(message)
-        f.close()
-        filename = 'file://' + os.getcwd() + '/index.html'
-        webbrowser.open_new_tab(filename)
-    except:
-        print("No result!")
-
-
-try:
-    ask_input()
-    elastic()
-    write_html()
-except:
-    print("Error! Possible errors:")
-    print("- File may not be exist. Make sure text.txt exists in this directory")
-    print("- If text.txt exists, make sure there are some text in it.")
-    print("- May be no result for this search") 
+    return message
